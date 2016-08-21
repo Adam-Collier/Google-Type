215e008c36d63c341d998846c1584cf73f5882bc
Adam-Collier <adamcollierart@gmail.com>
2016-08-21 20:12:09 +0100

    blah blah blah

--- js/functions.js
+++ js/functions.js
@@ -5,36 +5,21 @@ $(window).scroll(function(){
 
 var wScroll = $(this).scrollTop();
 
+var storyFade = [".johnny-town", ".squirrel-nutkin", ".benjamin-bunny" , ".two-bad-mice"];
 
-if(wScroll > $('.johnny-town').offset().top-500){
-	
-	$('.johnny-town').each(function(){
-		$('.johnny-town').children().addClass('is-showing');
-	});
-}
-
-	
-if(wScroll > $('.squirrel-nutkin').offset().top-500){
-	$('.squirrel-nutkin').each(function(){
-		$('.squirrel-nutkin').children().addClass('is-showing');
-	});
-}
-
-if(wScroll > $('.benjamin-bunny').offset().top-500){
-	$('.benjamin-bunny').each(function(){
-		$('.benjamin-bunny').children().addClass('is-showing');
-	});
-}
-
-if(wScroll > $('.two-bad-mice').offset().top-500){
-	$('.two-bad-mice').each(function(){
-		$('.two-bad-mice').children().addClass('is-showing');
-	});
-}
+storyFade.forEach(function(storyFade){
+   if(wScroll > $(storyFade).offset().top-500){
+   	$(storyFade).each(function(){
+        $(storyFade).children().addClass('is-showing');
+    });
+  }
+});
 
 //closing document ready and window scroll
 }); 
 });
+
+
 
 
 
